32d6fcaf153dbc4c9eeb2a608dc2b44de39e4f5d
EricJHelliwell <ericjhelliwell@gmail.com>
2024-06-07 13:57:19 +0300

    Readd Auth

--- amplify/auth/post-confirmation/handler.ts
+++ amplify/auth/post-confirmation/handler.ts
@@ -6,38 +6,9 @@ import {
 import { type Schema } from "../../data/resource";
 import { Amplify } from "aws-amplify";
 import { generateClient } from "aws-amplify/data";
-import { env } from '$amplify/env/post-confirmation';
 import { createUser } from "./graphql/mutations";
 
 const cogClient = new CognitoIdentityProviderClient();
-
-Amplify.configure(
-  {
-    API: {
-      GraphQL: {
-        endpoint: env.AMPLIFY_DATA_GRAPHQL_ENDPOINT,
-        region: env.AWS_REGION,
-        defaultAuthMode: 'userPool',
-      },
-    },
-  },
-  {
-    Auth: {
-      credentialsProvider: {
-        getCredentialsAndIdentityId: async () => ({
-          credentials: {
-            accessKeyId: env.AWS_ACCESS_KEY_ID,
-            secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
-            sessionToken: env.AWS_SESSION_TOKEN,
-          },
-        }),
-        clearCredentialsAndIdentityId: () => {
-          /* noop */
-        },
-      },
-    },
-  }
-);
 
 const dataClient = generateClient<Schema>({
   authMode: 'userPool',
@@ -46,7 +17,7 @@ const dataClient = generateClient<Schema>({
 // add user to group
 export const handler: PostConfirmationTriggerHandler = async (event) => {
   const command = new AdminAddUserToGroupCommand({
-    GroupName: env.GROUP_NAME,
+    GroupName: "EVERYONE",
     Username: event.userName,
     UserPoolId: event.userPoolId
   });
