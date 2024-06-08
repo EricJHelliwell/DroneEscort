cf216a939f9e56e053002161cb2719a632051030
EricJHelliwell <ericjhelliwell@gmail.com>
2024-06-08 12:06:28 +0300

    restore auth in post-confirmation

--- amplify/auth/post-confirmation/handler.ts
+++ amplify/auth/post-confirmation/handler.ts
@@ -7,10 +7,36 @@ import { type Schema } from "../../data/resource";
 import { Amplify } from "aws-amplify";
 import { generateClient } from "aws-amplify/data";
 import { createUser } from "./graphql/mutations";
-import outputs from '../../../amplify_outputs.json';
+import { env } from '$amplify/env/post-confirmation';
 
 const cogClient = new CognitoIdentityProviderClient();
-Amplify.configure(outputs);
+Amplify.configure(
+  {
+    API: {
+      GraphQL: {
+        endpoint: env.AMPLIFY_DATA_GRAPHQL_ENDPOINT,
+        region: env.AWS_REGION,
+        defaultAuthMode: 'userPool',
+      },
+    },
+  },
+  {
+    Auth: {
+      credentialsProvider: {
+        getCredentialsAndIdentityId: async () => ({
+          credentials: {
+            accessKeyId: env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+            sessionToken: env.AWS_SESSION_TOKEN,
+          },
+        }),
+        clearCredentialsAndIdentityId: () => {
+          /* noop */
+        },
+      },
+    },
+  }
+);
 
 const dataClient = generateClient<Schema>({
   authMode: 'iam',
